f1a4bf01cc1cc23f5606f3646abb1471e736bdff
Jean Carlo Nascimento <jnascimento@gmail.com>
2025-12-23 21:28:39 -0300

    🥷  ninjameeting

--- server/index.js
+++ server/index.js
@@ -67,7 +67,7 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 2000;
 server.listen(PORT, () => {
   console.log(`Signaling server running on port ${PORT}`);
 });
