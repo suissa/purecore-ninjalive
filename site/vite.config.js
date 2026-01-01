4686fbbad0476eee8fd36db3e0c0dfd41a381d4d
suissa <jnascimento@gmail.com>
2026-01-01 13:08:20 -0300

    allowedHosts

--- site/vite.config.js
+++ site/vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
     port: 5555,
     open: true,
     allowedHosts: [
-      'ninjameeting.purecore.codes',
-      'ninjameeting.suissai.dev',
+      'ninjalive.purecore.codes',
+      'ninjalive.suissai.dev',
       'localhost', '127.0.0.1', '0.0.0.0', '172.26.1.100']
   },
   preview: {
